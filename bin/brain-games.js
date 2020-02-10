3d99b41a45ab5224f856f282e826a635db75f892
DVo1 <dimytr.voitenko@gmail.com>
2020-02-10 18:22:04 +0200

    Just testing Actions are working?

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 import { hello, greeteng } from '../src/index.js';
 
-const name = hello();
-
 console.log('Welcome to the Brain Games!');
-console.log(greeteng() + name);
+console.log(greeteng() + hello());
