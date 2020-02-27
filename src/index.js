60b29e73bcdba0215019e56824e3ba648a07c846
DVo1 <dimytr.voitenko@gmail.com>
2020-02-27 21:12:51 +0200

    modified README.md

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 
 import readlineSync from 'readline-sync';
 
+const rounds = 3;
+
 const runGame = (description, getGameData) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
   console.log(description);
-  const round = 3;
-  for (let i = 0; i < round; i += 1) {
+  for (let i = 0; i < rounds; i += 1) {
     const [question, answer] = getGameData();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('You answer: ');
