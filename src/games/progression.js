51945567e41e97ea9fb1a97ff5caab48cb436a3d
DVo1 <dimytr.voitenko@gmail.com>
2020-02-29 14:56:52 +0200

    microedits

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,12 +4,7 @@ import runGame from '../index.js';
 const description = 'What number is missing in the progression?';
 const lengthProgression = 10;
 
-const getQueastion = (
-  firstMember,
-  difference,
-  progressionLength,
-  indexHiddeMember,
-) => {
+const getQueastion = (firstMember, difference, progressionLength, indexHiddeMember) => {
   let question = '';
   for (let i = 0; i < progressionLength; i += 1) {
     let currentMember = firstMember + i * difference;
@@ -23,12 +18,7 @@ const getGameData = () => {
   const indexHiddeMember = getRandomNum(0, lengthProgression - 1);
   const firstMember = getRandomNum(1, 100);
   const difference = getRandomNum(1, 100);
-  const question = getQueastion(
-    firstMember,
-    difference,
-    lengthProgression,
-    indexHiddeMember,
-  );
+  const question = getQueastion(firstMember, difference, lengthProgression, indexHiddeMember);
   const answer = String(firstMember + indexHiddeMember * difference);
   return [question, answer];
 };
